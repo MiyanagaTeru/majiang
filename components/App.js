d236b61f58f0b5a91c90bec2c7f84b5795eea112
Suinan Li <lsuinan@its.jnj.com>
2016-12-05 14:38:36 -0500

    init

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+				你好，世界。
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+// import individual reducers here
+
+const reducers = combineReducers({
+// individual reducers
+})
+
+export default reducers;
